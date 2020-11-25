c5e3e080f12346276fa2ebc132fdde28c55b6fb2
mechatroniks-git <57600622+mechatroniks-git@users.noreply.github.com>
2020-11-25 10:17:39 -0800

    Added state machine for LEDs

--- firmware/papracode/papracode.ino
+++ firmware/papracode/papracode.ino
@@ -1,9 +1,12 @@
 /*
   papracode
-  Test code to confirm the hardware
-  blink 4 LEDs
-  Read POT via ADC
-  Ouput PWM to fan
+    - Reads battery voltage and lights up LEDs to mimic M12 battery fuel gauge
+    - Reads user pot to control PWM of fan
+  to do
+    - look at averaging battery voltage
+    - set a MIN PWM value for fan output, vary with battery level
+  current code flash: 1764/4096 bytes (43%) RAM: 16/256 (6%) bytes without debug enabled
+               flash: 3169/4096 bytes (77%) with debug enabled  
  */
 
 // PA0 - UPDI/RESET
@@ -19,117 +22,160 @@
 // PB2 - NC
 // PB3 - LED4
 
-// Battery Voltage to Fuel Gauge 
-// > 4.12V/Cell = 100% - 78% Battery = 4 LEDs         => 960 > adc > 920
+// Battery Voltage to Fuel Gauge
+// > 4.12V/Cell = 100% - 78% Battery  = 4 LEDs         => 960 > adc > 920
 // > 3.95V/Cell =  77% - 55% Battery  = 3 LEDs         => 920 > adc > 862
 // > 3.70V/Cell =  54% - 33% Battery  = 2 LEDs         => 862 > adc > 825
 // > 3.54V/Cell =  32% - 10% Battery  = 1 LEDs         => 825 > adc > 757
 // > 3.25V/Cell =  10% - 0%  Battery  = LEDS FLASHING  => 757 > adc > 652
 // > 2.80V/Cell =  0%        Battery  = Shut Down
 
+//be sure to go to Arduino >> tools >> Support SerialEvent "No(saves space)" or "yes"
+//serial debug takes up about 1300 bytes of flash, out of 4096 available, not including print statements
+//#define DEBUG_SERIAL   //uncomment line to enable serial debug
 
 int led1 = PIN_PA5;
 int led2 = PIN_PA6;
 int led3 = PIN_PA7;
 int led4 = PIN_PB0;
-int offTime = 50;
-int onTime = 50;
-int analogPot = PIN_A2; //10 bit resolution
-int analogBatt = PIN_A1; 
+int analogBatt = PIN_A1; //10 bit resolution on ADC 
+int analogPot = PIN_A2; 
 int PWMPin = PIN_PA3;    //8 bit resolution with Arduino AnalogWrite
-int pot = 0; 
+int offTime = 95; //ms
+int onTime = 5;   //ms
+int loopDelay = 25; //ms
+int speedPot = 0;
 int battery = 0;
-int flashCounter = 0;
+int blinkCounter = 0;
+
+//State Machine states
+const int batteryCheck = 7;
+const int batteryFull  = 6;
+const int battery75p   = 5;
+const int battery50p   = 4;
+const int battery25p   = 3;
+const int battery10p   = 2;
+const int batteryDead  = 1;
+
+int batteryState = batteryCheck;
+
+const int LEDFlashLoop  = 25; //decrease for 10% battery LED to blink faster
+const int maxPWM = 255;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize the digital pin as an output.
-  Serial.begin(115200);
-  Serial.println("Starting up");
-
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
   pinMode(PWMPin, OUTPUT);
-   
+
   digitalWrite(led1, HIGH);
   digitalWrite(led2, HIGH);
   digitalWrite(led3, HIGH);
   digitalWrite(led4, HIGH);
-  digitalWrite(PWMPin, 255);
-  for (int i = 0; i <= 6; i++) {
-    digitalWrite(led1, LOW);  delay(onTime);   
-    digitalWrite(led1, HIGH); delay(offTime);           
-    digitalWrite(led2, LOW);  delay(onTime);            
-    digitalWrite(led2, HIGH); delay(offTime);           
-    digitalWrite(led3, LOW);  delay(onTime);            
-    digitalWrite(led3, HIGH); delay(offTime);           
-    digitalWrite(led4, LOW);  delay(onTime);            
+  digitalWrite(PWMPin, maxPWM); //Turn on fan 100%
+#ifdef DEBUG_SERIAL
+  Serial.begin(115200);
+  Serial.println("Starting up");
+#endif
+  for (int i = 0; i <= 6; i++) { //startup knightrider 
+    digitalWrite(led1, LOW);  delay(onTime);
+    digitalWrite(led1, HIGH); delay(offTime);
+    digitalWrite(led2, LOW);  delay(onTime);
+    digitalWrite(led2, HIGH); delay(offTime);
+    digitalWrite(led3, LOW);  delay(onTime);
+    digitalWrite(led3, HIGH); delay(offTime);
+    digitalWrite(led4, LOW);  delay(onTime);
     digitalWrite(led4, HIGH); delay(offTime);
-    digitalWrite(led3, LOW);  delay(onTime);            
-    digitalWrite(led3, HIGH); delay(offTime);           
-    digitalWrite(led2, LOW);  delay(onTime);            
-    digitalWrite(led2, HIGH); delay(offTime);           
-    digitalWrite(led1, LOW);  delay(onTime);            
+    digitalWrite(led3, LOW);  delay(onTime);
+    digitalWrite(led3, HIGH); delay(offTime);
+    digitalWrite(led2, LOW);  delay(onTime);
+    digitalWrite(led2, HIGH); delay(offTime);
+    digitalWrite(led1, LOW);  delay(onTime);
     digitalWrite(led1, HIGH); delay(offTime);
   }
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  pot = analogRead(analogPot) / 4;  // read the input pin, scale 10 bits to 8 bits
+  delay(25);
+  speedPot = analogRead(analogPot) >> 2;  // read the input pin, scale 10 bits to 8 bits
   battery = analogRead(analogBatt);
   switch (battery) {
-    case 0 ... 652: // Shutdown
-      digitalWrite(led1, HIGH);
-      digitalWrite(led2, HIGH);
-      digitalWrite(led3, HIGH);
-      digitalWrite(led4, HIGH);
-      pot = 0; // turn off fan     
-      break;
-    case 657 ... 757: // 10% - NEED TO FLASH 
-      digitalWrite(led2, HIGH);
-      digitalWrite(led3, HIGH);
-      digitalWrite(led4, HIGH);
-      if (flashCounter++ > 25) {
-        digitalWrite(led1, !digitalRead(led1));
-        flashCounter = 0; 
+    case 925 ... 1023: // Full = 78% - 100%
+      if (batteryState > batteryFull) {
+        batteryState = batteryFull;
+        digitalWrite(led1, LOW);  //All LEDs ON
+        digitalWrite(led2, LOW);
+        digitalWrite(led3, LOW);
+        digitalWrite(led4, LOW);
       }
       break;
-    case 762 ... 825: // 10% - 32%
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, HIGH);
-      digitalWrite(led3, HIGH);
-      digitalWrite(led4, HIGH);   
+    case 867 ... 924: // 75% = 55% - 77%
+      if (batteryState > battery75p) {
+        batteryState = battery75p;
+        digitalWrite(led1, LOW);  //3 LEDs ON
+        digitalWrite(led2, LOW);
+        digitalWrite(led3, LOW);
+        digitalWrite(led4, HIGH);
+      }
       break;
-    case 830 ... 862: // 33% - 54%
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      digitalWrite(led3, HIGH);
-      digitalWrite(led4, HIGH);   
+    case 830 ... 866: // 50% = 33% - 54%
+      if (batteryState > battery50p) {
+        batteryState = battery50p;
+        digitalWrite(led1, LOW);  //2 LEDs ON
+        digitalWrite(led2, LOW);
+        digitalWrite(led3, HIGH);
+        digitalWrite(led4, HIGH);
+      }
       break;
-    case 867 ... 920: // 55% - 77%
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      digitalWrite(led3, LOW);
-      digitalWrite(led4, HIGH);   
+    case 762 ... 829: // 25% = 10% - 32%
+      if (batteryState > battery25p) {
+        batteryState = battery25p;
+        digitalWrite(led1, LOW);  //1 LED on
+        digitalWrite(led2, HIGH);
+        digitalWrite(led3, HIGH);
+        digitalWrite(led4, HIGH);
+      }
       break;
-    case 925 ... 1023: // 78% - 100%
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      digitalWrite(led3, LOW);
-      digitalWrite(led4, LOW);   
+    case 657 ... 761: // 10% - Need to blink LED
+      if (batteryState > battery10p) {
+        batteryState = battery10p;
+        digitalWrite(led2, HIGH); //1 LED blinking
+        digitalWrite(led3, HIGH);
+        digitalWrite(led4, HIGH);
+        digitalWrite(led1, LOW);
+        blinkCounter = 0;  //reset the blink counter 
+      }
       break;
-    default:
-      // note: dead spots in case ranges are for hysteresis
+    case 0 ... 652: // Shutdown
+      if (batteryState > batteryDead) {
+        batteryState = batteryDead;
+        digitalWrite(led1, HIGH); //All LEDs off
+        digitalWrite(led2, HIGH);
+        digitalWrite(led3, HIGH);
+        digitalWrite(led4, HIGH);
+        speedPot = 0; // turn off fan - overrides the measuremnt from above
+      }
       break;
   }
-  analogWrite(PWMPin, pot);
-  Serial.print("Battery = "); 
-  Serial.print(battery); 
-  Serial.print(" pot ="); 
-  Serial.println(pot);
-  delay(25);
-  digitalWrite(PIN_PB1,!digitalRead(PIN_PB1));
+  analogWrite(PWMPin, speedPot);
+  // For battery state of 0-10%, need to blink LED1 to indicate almost dead battery
+  if (batteryState == battery10p){
+    if (blinkCounter++ > LEDFlashLoop) {
+      digitalWrite(led1, !digitalRead(led1));
+      blinkCounter = 0;
+    }
+  }  
+#ifdef DEBUG_SERIAL
+  //Serial.print("Battery = ");
+  //Serial.print(battery);
+  //Serial.print(" pot =");
+  //Serial.print(pot);
+  //Serial.print(" Battery State =");
+  //Serial.println(batteryState);
+#endif
+  //digitalWrite(PIN_PB1,!digitalRead(PIN_PB1)); //Toggle unused pin for debugging with scope 
 }
