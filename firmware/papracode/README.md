6cd08abde3d9c78e84a1ef8ff2b2492326dd0da6
mechatroniks-git <57600622+mechatroniks-git@users.noreply.github.com>
2021-01-09 11:28:48 -0800

    v0.2 PCB updates and knob speed reverse

---
+++ firmware/papracode/README.md
@@ -0,0 +1,6 @@
+# PAPRA CONTROLLER
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) ![testing badge](https://github.com/tetrabiodistributed/project-tetra-display/workflows/tests/badge.svg)
+
+A system to regulate fan speed from battery power, using M12 battery and 12V blower. Has battery fuel gauge display.
+
--- firmware/papracode/papracode.ino
+++ firmware/papracode/papracode.ino
@@ -5,35 +5,56 @@
   to do
     - look at averaging battery voltage
     - set a MIN PWM value for fan output, vary with battery level
-  current code flash: 2072/4096 bytes (50%) RAM: 30/256 (11%) bytes without debug enabled
-               flash: 3757/4096 bytes (91%) RAM: 93/256 (36%) with debug enabled  
+  AtTiny404
+  current code flash: 2204/4096 bytes (53%) RAM: 30/256 (11%) bytes without debug enabled
+               flash: 4021/4096 bytes (98%) RAM: 93/256 (36%) with debug enabled 
  */
 
 //be sure to go to Arduino >> tools >> Support SerialEvent "No(saves space)" or "yes"
 //serial debug takes up about 1300 bytes of flash, out of 4096 available, not including print statements
 #define DEBUG_SERIAL   //uncomment line to enable serial debug
 
-// Pin connections per PAPR V0.1 PCB
+// Pin connections per PAPR V0.2 PCB
 // PA0 - UPDI/RESET
 // PA1 - ADC - BATTERY
 // PA2 - ADC = POTENTIOMETER
 // PA3 - PWM - FAN
-// PA4 - FAN TACH INPUT
+// PA4 - EXT PWR SENSE 
 // PA5 - LED1
 // PA6 - LED2
 // PA7 - LED3
 // PB0 - UART RX
 // PB1 - UART TX
-// PB2 - NC
+// PB2 - FAN CONNECTION SENSE
 // PB3 - LED4
 
+int analogBatt = PIN_A1; //10 bit resolution on ADC 
+int analogPot = PIN_A2;  // Fan speed control POT with on/off switch
+int PWMPin = PIN_PA3;    //8 bit resolution with Arduino AnalogWrite
+int extPwrSense = PIN_PA4;
+int led1 = PIN_PA5;
+int led2 = PIN_PA6;
+int led3 = PIN_PA7;
+int led4 = PIN_PB0;
+int fanSense = PIN_PB1;
+
 // Battery Voltage to Fuel Gauge
-// > 4.12V/Cell = 100% - 78% Battery  = 4 LEDs         => 960 > adc > 920
-// > 3.95V/Cell =  77% - 55% Battery  = 3 LEDs         => 920 > adc > 862
-// > 3.70V/Cell =  54% - 33% Battery  = 2 LEDs         => 862 > adc > 825
-// > 3.54V/Cell =  32% - 10% Battery  = 1 LEDs         => 825 > adc > 757
-// > 3.25V/Cell =  10% - 0%  Battery  = LEDS FLASHING  => 757 > adc > 652
+// > 4.12V/Cell = 100% - 78% Battery  = 4 LEDs         => 899 > adc > 864
+// > 3.95V/Cell =  77% - 55% Battery  = 3 LEDs         => 864 > adc > 816
+// > 3.70V/Cell =  54% - 33% Battery  = 2 LEDs         => 816 > adc > 781
+// > 3.54V/Cell =  32% - 10% Battery  = 1 LEDs         => 781 > adc > 712
+// > 3.25V/Cell =  10% - 0%  Battery  = LEDS FLASHING  => 712 > adc > 618
 // > 2.80V/Cell =  0%        Battery  = Shut Down
+
+const int battADCMax  =  1023;
+const int battADCFull =   899;
+const int battADC78p  =   864;
+const int battADC55p  =   816;
+const int battADC33p  =   781;
+const int battADC10p  =   712;
+const int battADC0p   =   618;
+const int battADCMin  =     0;
+
 
 //Limits the min pot value     Voltage   Min PWM Percentage
 const int minPWMfull =  69;  //11.85V      69     >77%
@@ -54,13 +75,6 @@ const int batteryDead  = 1;
 
 int batteryState = batteryCheck;
 
-int led1 = PIN_PA5;
-int led2 = PIN_PA6;
-int led3 = PIN_PA7;
-int led4 = PIN_PB0;
-int analogBatt = PIN_A1; //10 bit resolution on ADC 
-int analogPot = PIN_A2; 
-int PWMPin = PIN_PA3;    //8 bit resolution with Arduino AnalogWrite
 int offTime = 5; //ms
 int onTime = 95;   //ms
 int loopDelay = 25; //ms
@@ -68,7 +82,8 @@ int blinkCounter = 0;
 const uint32_t numBatterySamples = 10;
 uint32_t battery = 1023;
 const uint32_t maxPot = 1023;
-uint32_t speedPot = 0;
+const uint32_t minPot = 0;
+uint32_t fanPWM = 0;
 uint32_t minPWM = minPWM10p;
 uint32_t maxPWM = 255;
 uint32_t rawADC = 0;
@@ -83,6 +98,8 @@ void setup() {
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
   pinMode(PWMPin, OUTPUT);
+  pinMode(fanSense, INPUT);
+  pinMode(extPwrSense, INPUT);  
 
   digitalWrite(led1, HIGH);
   digitalWrite(led2, HIGH);
@@ -92,6 +109,10 @@ void setup() {
 #ifdef DEBUG_SERIAL
   Serial.begin(115200);
   Serial.println("Starting up");
+  Serial.println("PAPRA 09JAN2021");
+  Serial.println("PCB v0.2");
+  Serial.println("AtTiny 404");
+  Serial.println("(c) Tetra Bio Distributed 2021");
 #endif
   
   for (int i = 0; i <= 4; i++) { //startup knightrider 
@@ -118,14 +139,15 @@ void loop() {
   //Clamp and rescale potentiometer input from a 10bit value to 8 bit
   if (maxPWM > 0) {
     rawADC = analogRead(analogPot);
-    speedPot = ( ( ( maxPWM - minPWM ) * rawADC ) / maxPot ) + minPWM;
-  } else {
-    speedPot = maxPWM;
+    fanPWM = map( rawADC, minPot, maxPot, maxPWM, minPWM );
+  } 
+  else {
+    fanPWM = maxPWM;
   }  
-  analogWrite(PWMPin, speedPot);
+  analogWrite(PWMPin, fanPWM);
   battery = battery = ( ( battery * ( numBatterySamples - 1 ) ) + analogRead(analogBatt) ) / numBatterySamples;
   switch (battery) {
-    case 925 ... 1023: // Full = 78% - 100%
+    case battADC78p ... battADCMax: // Full = 78% - 100%
       if (batteryState > batteryFull) {
         batteryState = batteryFull;
         digitalWrite(led1, LOW);  //All LEDs ON
@@ -135,7 +157,7 @@ void loop() {
         minPWM = minPWMfull;
       }
       break;
-    case 867 ... 924: // 75% = 55% - 77%
+    case battADC55p ... ( battADC78p - 1 ): // 75% = 55% - 77%
       if (batteryState > battery75p) {
         batteryState = battery75p;
         digitalWrite(led1, LOW);  //3 LEDs ON
@@ -145,7 +167,7 @@ void loop() {
         minPWM = minPWM75p;
       }
       break;
-    case 830 ... 866: // 50% = 33% - 54%
+    case battADC33p ... ( battADC55p - 1 ): // 50% = 33% - 54%
       if (batteryState > battery50p) {
         batteryState = battery50p;
         digitalWrite(led1, LOW);  //2 LEDs ON
@@ -155,7 +177,7 @@ void loop() {
         minPWM = minPWM50p;
       }
       break;
-    case 762 ... 829: // 25% = 10% - 32%
+    case battADC10p ... ( battADC33p -1 ): // 25% = 10% - 32%
       if (batteryState > battery25p) {
         batteryState = battery25p;
         digitalWrite(led1, LOW);  //1 LED on
@@ -165,7 +187,7 @@ void loop() {
         minPWM = minPWM25p;
       }
       break;
-    case 657 ... 761: // 10% - Need to blink LED
+    case battADC0p ... ( battADC10p - 1) : // 10% - Need to blink LED
       if (batteryState > battery10p) {
         batteryState = battery10p;
         digitalWrite(led2, HIGH); //1 LED blinking
@@ -175,7 +197,7 @@ void loop() {
         minPWM = minPWM10p;
       }
       break;
-    case 0 ... 652: // Shutdown
+    case battADCMin ... (battADC0p - 1 ): // Shutdown
       if (batteryState > batteryDead) {
         batteryState = batteryDead;
         digitalWrite(led1, HIGH); //All LEDs off
@@ -194,10 +216,11 @@ void loop() {
     }
   }  
 #ifdef DEBUG_SERIAL
-  Serial.print(" Battery = ");      Serial.print(battery);
-  Serial.print(" rawADC =");        Serial.print(rawADC);
-  Serial.print(" pot =");           Serial.print(speedPot);
-  Serial.print(" Battery State ="); Serial.println(batteryState);
-  digitalWrite(PIN_PB1,!digitalRead(PIN_PB1)); //Toggle unused pin for debugging with scope 
+  Serial.print("fs = ");             Serial.print( digitalRead( fanSense ) );
+  Serial.print(" eps = ");           Serial.print( digitalRead( extPwrSense ) );
+  Serial.print(" Battery = ");       Serial.print(battery);
+  Serial.print(" rawADC = ");        Serial.print(rawADC);
+  Serial.print(" pwm = ");           Serial.print(fanPWM);
+  Serial.print(" Battery State = "); Serial.println(batteryState);
 #endif
 }
